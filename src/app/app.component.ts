efa514e18b76af94c7aa756db4b8f73f1cfbd97e
deepakkumar07-debug <deepakkumar.sekar99@gmail.com>
2024-07-25 17:04:13 +0530

    Angular Components introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  constructor() {
+    console.log('App Component Constructor Called.');
+  }
+
+  username = 'Deepak';
+  imageUrl: string =
+    'https://angular.io/assets/images/logos/angular/angular.png';
+
+  greet() {
+    alert('Hello, Angular!');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @NgModule({
   declarations: [
-    AppComponent
+    // angular automatically updates whenever you create an new component using ng command,
+    //if not you have to register here by importing the component
+    AppComponent,
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
