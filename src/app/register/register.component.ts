902fc450a4ea79790e7e7144182f50c870a670d9
deepakkumar07-debug <deepakkumar.sekar99@gmail.com>
2024-08-02 15:59:36 +0530

    reactive forms basic

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,7 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  saveRegisterForm(registerForm: any) {
-    console.log('Register Form Value', registerForm.value);
+  registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      emailId: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      gender: ['', Validators.required],
+      hobbies: this.fb.group({
+        reading: [false],
+        traveling: [false],
+        cooking: [false],
+      }),
+      country: ['', Validators.required],
+      dob: ['', Validators.required],
+    });
+  }
+
+  saveRegisterForm(): void {
+    if (this.registerForm.valid) {
+      console.log('Form Submitted!', this.registerForm.value);
+    }
   }
 }
